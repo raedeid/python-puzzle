33ebffae31e99b5c715065af419d22919004e5c8
raedeid <43578370+raedeid@users.noreply.github.com>
2019-03-16 22:37:55 +0200

    Add files via upload

---
+++ proplem1.py
@@ -0,0 +1,14 @@
+a=3
+sum_1=0
+b=5
+c=1
+while a<1000 or b<1000 :
+    if a<1000 or b<1000:
+        a=3
+        b=5
+        a=c*a
+        b=c*b
+        summ=a+b
+    sum_1=summ+sum_1 
+    c=c+1
+print(sum_1)    
