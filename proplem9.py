7213f9eac34c55d681adcb1bc1dce8a1277cd6ad
raedeid <43578370+raedeid@users.noreply.github.com>
2019-04-13 23:50:39 +0300

    Add files via upload

---
+++ proplem9.py
@@ -0,0 +1,25 @@
+#A Pythagorean triplet is a set of three natural numbers, a < b < c, for which,
+
+#a2 + b2 = c2
+#For example, 32 + 42 = 9 + 16 = 25 = 52.
+
+#There exists exactly one Pythagorean triplet for which a + b + c = 1000.
+#Find the product abc.
+
+
+def is_triple_of_Pythagorean(a,b,c):#check if the 3 number has feature of the pythagorean trriple
+    if first**2+seconed**2==third**2:
+        return True
+    return False
+
+
+
+for first in range(1,1000):#make roll for number under cause we need sumation equal 1000
+    for seconed in range (1,1000):
+        for third in range (1,1000):
+            if first+seconed+third==1000:
+                if is_triple_of_Pythagorean(first,seconed,third):
+                    print (first*seconed*third)#print the product
+           
+
+        
