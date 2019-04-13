d32e112a3960204d83076653ddfa40568db7849b
raedeid <43578370+raedeid@users.noreply.github.com>
2019-04-14 01:21:01 +0300

    Add files via upload

---
+++ proplem10 ten.py
@@ -0,0 +1,20 @@
+#The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
+
+#Find the sum of all the primes below two million.
+import math
+def is_prime(number):
+    for b in range(2,int(math.sqrt(number))+1):
+        if number%b==0:
+            return True
+    return False
+
+
+list_of_primes=[]
+for Number in range(2,2000000):
+    if is_prime(Number):
+        list_of_primes.append(Number)
+
+
+
+print(sum(list_of_primes))        
+
