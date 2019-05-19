fbab03269a189bb213b0f287519e75b6984a6882
raedeid <43578370+raedeid@users.noreply.github.com>
2019-05-19 23:22:01 +0300

    Update proplem4.py

--- proplem4.py
+++ proplem4.py
@@ -1,15 +1,16 @@
-#A palindromic number reads the same both ways. The largest palindrome made from the product of two 2-digit numbers is 9009 = 91 × 99.
-
-#Find the largest palindrome made from the product of two 3-digit numbers
 def both_way(a):#test if result of production is palindrome
     b=list(str(a))
-    if b[:]==b[::-1]:#find if the number equal his inverse 
+    if b[:]==b[::-1]:#find if the number equal his inverse
         return True
     return False
 
-for c in range(100,10,-1):#loop for 2 digit number 
-    for d in range(1000,100,-1):#loop for 3 digit number 
+
+g=[]
+for c in range(999,99,-1):#loop for 2 digit number
+    for d in range(999,99,-1):#loop for 3 digit number
        production=c*d
        if both_way(production):
-          print(production,'=',c,'*',d)
+          g.append(production)
           break  
+
+print(max(g))
