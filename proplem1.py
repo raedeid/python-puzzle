84294290a60e07a006ad5d2c4ef1ddcddc65a4ba
raedeid <43578370+raedeid@users.noreply.github.com>
2019-05-20 04:05:21 +0300

    Update proplem1.py

--- proplem1.py
+++ proplem1.py
@@ -1,14 +1,5 @@
-a=3
-sum_1=0
-b=5
-c=1
-while a<1000 or b<1000 :
-    if a<1000 or b<1000:
-        a=3
-        b=5
-        a=c*a
-        b=c*b
-        summ=a+b
-    sum_1=summ+sum_1 
-    c=c+1
-print(sum_1)    
+g=[]
+for a in range (1000):
+    if a%3==0 and a%5==0:
+        g.append(a)
+print(sum(g))        
